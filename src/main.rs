e87dfaee92bf9f3064ffdfef326526aed1344b4d
Balazs Horvath <acsipont@gmail.com>
2024-05-20 16:11:03 +0200

    awoo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use rand::prelude::*;
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
 use bevy::prelude::*;
 use bevy::utils::Duration;
 use bevy::input::mouse::MouseButtonInput;
@@ -8,8 +10,66 @@ use bevy::input::ButtonState;
 use bevy::ecs::system::lifetimeless::SRes;
 use bevy::ecs::system::SystemParam;
 use bevy::window::PrimaryWindow;
-use iyes_perf_ui::prelude::*;
-use iyes_cli::prelude::*;
+
+use perf_ui::prelude::*;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((Camera2dBundle::default(), MainCamera));
+
+    // Performance UI
+    commands.spawn((
+        PerfUiRoot {
+            font_label: asset_server.load("bahnschrift.ttf"),
+            font_value: asset_server.load("bahnschrift.ttf"),
+            font_highlight: asset_server.load("bahnschrift.ttf"),
+            values_col_width: Some(80.0),
+            ..default()
+        },
+        PerfUiEntryFPS::default(),
+        PerfUiTimeSinceLastClick::default(),
+        PerfUiTimeSinceLastKeypress::default(),
+        PerfUiSpaceKeyPressCount::default(),
+    ));
+
+    // 🐈‍⬛
+    let cat_name = generate_cat_name();
+    let _cat_entity = commands.spawn((
+        // Marker
+        Cat {
+            name: cat_name.clone(),
+        },
+        // A 2D sprite
+        SpriteBundle {
+            texture: asset_server.load("cat.png"),
+            transform: Transform::from_xyz(25.0, 50.0, 0.0),
+            // use the default values for all other components in the bundle
+            ..Default::default()
+        }
+    ));
+
+    // Print the cat's name
+    println!("The cat's name is: {}", cat_name);
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
+        .add_plugins(PerfUiPlugin)
+        .add_perf_ui_entry_type::<PerfUiTimeSinceLastClick>()
+        .add_perf_ui_entry_type::<PerfUiTimeSinceLastKeypress>()
+        .add_perf_ui_entry_type::<PerfUiSpaceKeyPressCount>()
+        .init_resource::<CursorWorldCoordinates>()
+        .init_resource::<TimeSinceLastClick>()
+        .init_resource::<TimeSinceLastKeypress>()
+        .init_resource::<SpaceKeyPressCount>()
+        .add_systems(Startup, setup)
+        .add_systems(Update, cursor_system)
+        .add_systems(Update, handle_click)
+        .add_systems(Update, handle_keypress)
+        .add_systems(Update, handle_space_keypress)
+        .run();
+}
 
 /**
  * Stores the world position of the mouse cursor.
@@ -35,11 +95,47 @@ pub struct TimeSinceLastKeypress {
 
 
 /**
+ * The Health Component 🩸 
+ */
+#[derive(Component)]
+struct Health {
+    current: u32,
+    max: u32,
+}
+
+/**
  * The Cat Component 🐈‍⬛
  */
 #[derive(Component)]
 struct Cat {
     name: String,
+}
+
+fn generate_cat_name() -> String {
+    let mut rng = thread_rng();
+    let cat_names = vec![
+        "picard", "beverly", "data", "troi", "laforge", "crusher", "yar", "kirk",
+        "spock", "mccoy", "scotty", "uhura", "sulu", "chekov", "chakotay", "tuvok",
+        "sisko", "kira", "dax", "bashir", "odo", "quark", "archer", "tucker",
+        "tpol", "reed", "mayweather", "phlox", "sato", "sevenofnine", "thedoctor",
+        "tomparis", "harrykim", "belanna", "torres", "jeanluc", "lorca", "burnham",
+        "saru", "stamets", "tilly", "georgiou", "culber", "cornwell", "leland",
+        "vance", "reno", "booker", "grudge", "shaxs", "detmer", "owosekun", "rhys",
+        "pike", "number-one", "laan", "chapel", "kyle", "vina", "mudd", "garak",
+        "leyton", "ross", "nog", "jake", "seven", "janeway", "tuvix", "neelix",
+        "kes", "carey", "vorik", "wildman", "zahir", "seska", "jonas", "rio",
+        "maxwell", "tryla", "lorian", "icheb", "q", "guinan", "pulaski", "ro",
+        "hwomyn", "riker", "shelby", "obrien", "keiko", "molly", "kirayoshi",
+        "naomi", "ezri", "kassidy", "leeta", "nog", "rom", "brunt", "ishka", "worf",
+        "martok", "grilka", "sharan", "alexander", "kehleyr", "lwaxana", "kamala",
+        "vash", "tasha", "ogawa", "barclay", "maddox", "soong", "juliana", "sela",
+        "toral", "ziyal", "dukat", "damar", "weyoun", "eddington", "michael",
+        "sarina", "hugh", "lore", "el-aurian"
+    ];
+
+
+    let name = cat_names.choose(&mut rng).unwrap().to_string();
+    return name;
 }
 
 /**
@@ -79,7 +175,7 @@ impl Default for PerfUiTimeSinceLastClick {
             color_gradient: ColorGradient::new_preset_gyr(1.0, 4.0, 8.0).unwrap(),
             digits: 2,
             precision: 3,
-            sort_key: iyes_perf_ui::utils::next_sort_key(),
+            sort_key: perf_ui::utils::next_sort_key(),
         }
     }
 }
@@ -93,7 +189,7 @@ impl Default for PerfUiTimeSinceLastKeypress {
             color_gradient: ColorGradient::new_preset_gyr(1.0, 4.0, 8.0).unwrap(),
             digits: 2,
             precision: 3,
-            sort_key: iyes_perf_ui::utils::next_sort_key(),
+            sort_key: perf_ui::utils::next_sort_key(),
         }
     }
 }
@@ -108,7 +204,7 @@ impl Default for PerfUiSpaceKeyPressCount {
     fn default() -> Self {
         PerfUiSpaceKeyPressCount {
             label: String::new(),
-            sort_key: iyes_perf_ui::utils::next_sort_key(),
+            sort_key: perf_ui::utils::next_sort_key(),
         }
     }
 }
@@ -170,7 +266,7 @@ impl PerfUiEntry for PerfUiTimeSinceLastClick {
     }
 
     fn format_value(&self, value: &Self::Value) -> String {
-        let mut s = iyes_perf_ui::utils::format_pretty_float(self.digits, self.precision, *value);
+        let mut s = perf_ui::utils::format_pretty_float(self.digits, self.precision, *value);
         if self.display_units {
             s.push_str(" s");
         }
@@ -178,7 +274,7 @@ impl PerfUiEntry for PerfUiTimeSinceLastClick {
     }
 
     fn width_hint(&self) -> usize {
-        let w = iyes_perf_ui::utils::width_hint_pretty_float(self.digits, self.precision);
+        let w = perf_ui::utils::width_hint_pretty_float(self.digits, self.precision);
         if self.display_units {
             w + 2
         } else {
@@ -222,7 +318,7 @@ impl PerfUiEntry for PerfUiTimeSinceLastKeypress {
     }
 
     fn format_value(&self, value: &Self::Value) -> String {
-        let mut s = iyes_perf_ui::utils::format_pretty_float(self.digits, self.precision, *value);
+        let mut s = perf_ui::utils::format_pretty_float(self.digits, self.precision, *value);
         if self.display_units {
             s.push_str(" s");
         }
@@ -230,7 +326,7 @@ impl PerfUiEntry for PerfUiTimeSinceLastKeypress {
     }
 
     fn width_hint(&self) -> usize {
-        let w = iyes_perf_ui::utils::width_hint_pretty_float(self.digits, self.precision);
+        let w = perf_ui::utils::width_hint_pretty_float(self.digits, self.precision);
         if self.display_units {
             w + 2
         } else {
@@ -297,229 +393,13 @@ fn cursor_system(
     // There is only one primary window, so we can get it from the query:
     let window = window_query.single();
 
-    // check if the cursor is inside the window and get its position
-    // then, ask bevy to convert into world coordinates, and truncate to discard Z
+    // Check if the cursor is inside the window and get its position
+    // then, ask bevy to convert into world coordinates, and truncate to discard Z.
     if let Some(world_position) = window.cursor_position()
         .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
         .map(|ray| ray.origin.truncate())
     {
         coords.0 = world_position;
-        eprintln!("CursorWorldCoordinate: {}/{}", world_position.x, world_position.y);
-    }
-}
-
-#[derive(Component)]
-struct DespawnTimeout(Timer);
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>, world: &mut World) {
-    commands.spawn((Camera2dBundle::default(), MainCamera));
-    let camera = MainCamera;
-    world.spawn(camera);
-
-    let cat_names = vec![
-        "picard", "beverly", "data", "troi", "laforge", "crusher", "yar", "kirk",
-    ];
-
-    let mut rng = thread_rng();
-
-    let name = cat_names.choose(&mut rng).unwrap().to_string();
-    commands.spawn((Cat { name: name.clone() },));
-    println!("Cat name: {}", name);
-
-    commands.spawn((
-        PerfUiRoot {
-            font_label: asset_server.load("bahnschrift.ttf"),
-            font_value: asset_server.load("bahnschrift.ttf"),
-            font_highlight: asset_server.load("bahnschrift.ttf"),
-            values_col_width: Some(80.0),
-            ..default()
-        },
-        PerfUiEntryFPS::default(),
-        PerfUiTimeSinceLastClick::default(),
-        PerfUiTimeSinceLastKeypress::default(),
-        PerfUiSpaceKeyPressCount::default(),
-    ));
-}
-
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
-        .add_plugins(PerfUiPlugin)
-        .add_perf_ui_entry_type::<PerfUiTimeSinceLastClick>()
-        .add_perf_ui_entry_type::<PerfUiTimeSinceLastKeypress>()
-        .add_perf_ui_entry_type::<PerfUiSpaceKeyPressCount>()
-        .init_resource::<CursorWorldCoordinates>()
-        .init_resource::<TimeSinceLastClick>()
-        .init_resource::<TimeSinceLastKeypress>()
-        .init_resource::<SpaceKeyPressCount>()
-        .register_clicommand_noargs("version", show_version)
-        .register_clicommand_noargs("help", show_help)
-        .register_clicommand_noargs("spawn", spawn_sprite_random)
-        .register_clicommand_args("spawn", spawn_sprite_at)
-        .register_clicommand_noargs("despawn", despawn_sprites)
-        .add_systems(Startup, (setup, setup_console))
-        .add_systems(Update, (mouseclicks, console_text_input, despawn_timeout))
-        .add_systems(Update, cursor_system)
-        .add_systems(Update, handle_click)
-        .add_systems(Update, handle_keypress)
-        .add_systems(Update, handle_space_keypress)
-        .run();
-}
-
-fn setup_console(world: &mut World) {
-    let font = world.resource::<AssetServer>().load("Ubuntu-R.ttf");
-    let console = world
-        .spawn(NodeBundle {
-            style: Style {
-                position_type: PositionType::Absolute,
-                bottom: Val::Percent(5.0),
-                left: Val::Percent(5.0),
-                top: Val::Auto,
-                right: Val::Auto,
-                padding: UiRect::all(Val::Px(8.0)),
-                align_items: AlignItems::Center,
-                ..Default::default()
-            },
-            background_color: BackgroundColor(Color::BEIGE),
-            ..Default::default()
-        })
-        .id();
-    let prompt_style = TextStyle {
-        font: font.clone(),
-        font_size: 24.0,
-        color: Color::RED,
-    };
-    let input_style = TextStyle {
-        font: font.clone(),
-        font_size: 16.0,
-        color: Color::BLACK,
-    };
-    let prompt = world
-        .spawn((
-            CliPrompt,
-            TextBundle {
-                text: Text::from_sections([
-                    TextSection::new("~ ", prompt_style),
-                    TextSection::new("", input_style),
-                ]),
-                ..Default::default()
-            },
-        ))
-        .id();
-    world.entity_mut(console).push_children(&[prompt]);
-}
-
-#[derive(Component)]
-struct CliPrompt;
-
-fn show_version() {
-    println!("TODO");
-}
-
-fn spawn_sprite_random(q_window: Query<&Window, With<PrimaryWindow>>, mut commands: Commands) {
-    let window = q_window.single();
-    let mut rng = thread_rng();
-    commands.spawn((
-        DespawnTimeout(Timer::new(Duration::from_secs(5), TimerMode::Once)),
-        SpriteBundle {
-            sprite: Sprite {
-                color: Color::PINK,
-                custom_size: Some(Vec2::splat(64.0)),
-                ..default()
-            },
-            transform: Transform::from_xyz(
-                rng.gen_range(0.0..window.width()),
-                rng.gen_range(0.0..window.height()),
-                1.0,
-            ),
-            ..default()
-        },
-    ));
-}
-
-fn spawn_sprite_at(In(args): In<Vec<String>>, mut commands: Commands) {
-    if args.len() != 2 {
-        error!("spawn command must take exactly 2 args!");
-        return;
-    }
-    let Ok(x) = args[0].parse::<f32>() else {
-        error!("spawn command args must be numbers!");
-        return;
-    };
-    let Ok(y) = args[1].parse::<f32>() else {
-        error!("spawn command args must be numbers!");
-        return;
-    };
-
-    commands.spawn((
-        DespawnTimeout(Timer::new(Duration::from_secs(5), TimerMode::Once)),
-        SpriteBundle {
-            sprite: Sprite {
-                color: Color::PINK,
-                custom_size: Some(Vec2::splat(64.0)),
-                ..default()
-            },
-            transform: Transform::from_xyz(x, y, 1.0),
-            ..default()
-        },
-    ));
-}
-
-fn despawn_sprites(mut commands: Commands, q: Query<Entity, With<Sprite>>) {
-    for e in &q {
-        commands.entity(e).despawn();
-    }
-}
-
-fn show_help(world: &mut World) {
-    let font = world.resource::<AssetServer>().load("bahnschrift.ttf");
-    let help_box = world
-        .spawn((
-            DespawnTimeout(Timer::new(Duration::from_secs(5), TimerMode::Once)),
-            NodeBundle {
-                style: Style {
-                    position_type: PositionType::Absolute,
-                    top: Val::Percent(5.0),
-                    left: Val::Percent(5.0),
-                    bottom: Val::Auto,
-                    right: Val::Auto,
-                    padding: UiRect::all(Val::Px(8.0)),
-                    align_items: AlignItems::Center,
-                    ..Default::default()
-                },
-                background_color: BackgroundColor(Color::BEIGE),
-                ..Default::default()
-            },
-        ))
-        .id();
-    let text_style = TextStyle {
-        font: font.clone(),
-        font_size: 12.0,
-        color: Color::BLACK,
-    };
-    let prompt = world
-        .spawn((TextBundle {
-            text: Text::from_section(
-                "Available console commands: \"help\", \"hello\", \"spawn\", \"spawn <x> <y>\", \"despawn\".\n
-                Left/Right mouse click will run \"spawn\"/\"despawn\".",
-                text_style,
-            ),
-            ..Default::default()
-        },))
-        .id();
-    world.entity_mut(help_box).push_children(&[prompt]);
-}
-
-fn despawn_timeout(
-    mut commands: Commands,
-    t: Res<Time>,
-    mut q: Query<(Entity, &mut DespawnTimeout)>,
-) {
-    for (e, mut timeout) in &mut q {
-        timeout.0.tick(t.delta());
-        if timeout.0.finished() {
-            commands.entity(e).despawn_recursive();
-        }
+        //eprintln!("CursorWorldCoordinate: {}/{}", world_position.x, world_position.y);
     }
 }
