4572073c2ae60e823e36d7cb7e08059475094b62
Gaeros <163143799+elias-gaeros@users.noreply.github.com>
2024-06-02 13:09:21 +0200

    smooth_pixel_camera

--- src/components/gamestate.rs
+++ src/components/gamestate.rs
@@ -26,9 +26,9 @@ pub fn set_state_mainmenu() {
 
 pub fn set_state_playing(mut commands: Commands, asset_server: Res<AssetServer>) {
     info!("Set GameState: Playing");
+    // We have to move the camera to the menu once added
     // 🎥
-    let camera = Camera2dBundle::default();
-    commands.spawn(camera);
+    commands.spawn(super::camera::camera_bundle());
     super::systems::spawn_ldtk_world(commands, asset_server);
 }
 
