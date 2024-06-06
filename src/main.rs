d7cef4250de25cbaf724db9761d9c0027a4d0b4b
Gaeros <163143799+elias-gaeros@users.noreply.github.com>
2024-06-06 11:50:36 +0200

    move debug plugins to conditional mod

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,6 @@ use wgpu::Backends;
 mod components;
 mod plugins;
 
-#[cfg(debug_assertions)]
-use bevy::diagnostic::{ FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin };
-
-use bevy::log::LogPlugin;
 use bevy::prelude::*;
 use bevy_tweening::*;
 use bevy_steamworks::*;
@@ -26,12 +22,6 @@ use bevy_steamworks::*;
 // ⚠️ TODO: This will need to get eventually removed from main.
 // use components::player::Player;
 use components::torch::Torch;
-//
-
-#[cfg(debug_assertions)]
-use bevy::input::common_conditions::input_toggle_active;
-#[cfg(debug_assertions)]
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 // ⚠️ TODO: Move audio stuff to its own thing
 use bevy::audio::{ SpatialScale, AudioPlugin };
@@ -120,22 +110,6 @@ fn main() {
 
     //app.add_systems(Startup, play_background_audio);
 
-    // this code is compiled only if debug assertions are enabled (debug mode)
-    #[cfg(debug_assertions)]
-    let log_plugin = LogPlugin {
-        level: bevy::log::Level::DEBUG,
-        filter: "info,pattern=debug,wgpu_core=warn,wgpu_hal=warn,pattern=debug".into(),
-        update_subscriber: None,
-    };
-
-    // this code is compiled only if debug assertions are disabled (release mode)
-    #[cfg(not(debug_assertions))]
-    let log_plugin = LogPlugin {
-        level: bevy::log::Level::INFO,
-        filter: "warning,pattern=info".into(),
-        update_subscriber: None,
-    };
-
     #[cfg(target_arch = "wasm32")]
     let window_plugin = WindowPlugin {
         primary_window: Some(Window {
@@ -163,7 +137,7 @@ fn main() {
             })
                 .set(window_plugin)
                 .set(ImagePlugin::default_nearest())
-                .set(log_plugin)
+                .set(plugins::debug::make_log_plugin())
                 // ⚠️ TODO: Maybe move this to its own thing? I'm not sure!
                 .set(AudioPlugin {
                     default_spatial_scale: SpatialScale::new_2d(AUDIO_SCALE),
@@ -174,26 +148,11 @@ fn main() {
             components::gamestate::game_state_plugin,
             components::ui::setup_ui,
             components::systems::setup_ldtk,
+            plugins::debug::plugin,
         ))
         .insert_resource(GlobalVolume::new(0.2))
         .add_systems(Startup, change_global_volume);
     //.add_systems(Startup, play_2d_spatial_audio);
-
-    #[cfg(debug_assertions)]
-    app.add_plugins((
-        // FrameTimeDiagnosticsPlugin
-        bevy::diagnostic::FrameTimeDiagnosticsPlugin,
-        // LogDiagnosticsPlugin
-        LogDiagnosticsPlugin::default(),
-        // EntityCountDiagnosticsPlugin
-        bevy::diagnostic::EntityCountDiagnosticsPlugin::default(),
-        // SystemInformationDiagnosticsPlugin
-        bevy::diagnostic::SystemInformationDiagnosticsPlugin::default(),
-        // WorldInspectorPlugin
-        WorldInspectorPlugin::default().run_if(input_toggle_active(false, KeyCode::F11)),
-        // PerformanceUI
-        components::perfui::setup_perf_ui,
-    ));
 
     app.run();
 }
