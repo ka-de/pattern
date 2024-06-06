155efd46b0692131895c0878f7e6e1431c2d3268
Balazs Horvath <acsipont@gmail.com>
2024-06-06 11:05:07 +0200

    Icon and diagnostics

--- build.rs
+++ build.rs
@@ -6,6 +6,6 @@ fn main() {
     let target = env::var("TARGET").unwrap();
     if target.contains("windows") {
         // Sets the game icon in the executable for Windows.
-        embed_resource::compile("build/windows/icon.rc");
+        embed_resource::compile("build/windows/icon.rc", embed_resource::NONE);
     }
 }
