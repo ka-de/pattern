f1f2899a41563061f7cf6a23a3d6e335a7f6cc86
Balazs Horvath <acsipont@gmail.com>
2024-06-08 22:47:12 +0200

    changes

--- src/components/collision.rs
+++ src/components/collision.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
-use bevy_rapier2d::{ prelude::*, rapier::geometry::InteractionGroups };
+use bevy_rapier2d::prelude::*;
 
 #[derive(Clone, Default, Bundle, LdtkIntCell)]
 pub struct ColliderBundle {
@@ -24,7 +24,7 @@ impl From<&EntityInstance> for ColliderBundle {
         match entity_instance.identifier.as_ref() {
             "Player" =>
                 ColliderBundle {
-                    collider: Collider::cuboid(12.0, 12.0),
+                    collider: Collider::cuboid(8.0, 12.0),
                     friction: Friction {
                         coefficient: 0.0,
                         combine_rule: CoefficientCombineRule::Min,
