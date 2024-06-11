2cd0463059ab54ced76370b3a4c2f7a794768f4d
Balazs Horvath <acsipont@gmail.com>
2024-06-11 23:45:11 +0200

    even less spatial audio scale

--- src/plugins/audio/mod.rs
+++ src/plugins/audio/mod.rs
@@ -4,7 +4,7 @@ mod insert_audio_components;
 mod play_background_music;
 pub(crate) mod change_global_volume;
 
-const AUDIO_SCALE: f32 = 1.0 / 50.0;
+const AUDIO_SCALE: f32 = 1.0 / 25.0;
 
 pub fn plugin(app: &mut App) {
     app.add_systems(PreUpdate, (
