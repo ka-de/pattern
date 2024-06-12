a8c0857e010d81ef611a22d1908b8f65b664acbe
Gaeros <163143799+elias-gaeros@users.noreply.github.com>
2024-06-12 16:15:43 +0200

    delayed looped audio playback

--- src/plugins/audio/insert_audio_components.rs
+++ src/plugins/audio/insert_audio_components.rs
@@ -1,12 +1,16 @@
+use std::time::Duration;
+
 use bevy::{
     asset::AssetServer,
-    audio::{ AudioBundle, PlaybackSettings, SpatialListener, SpatialScale },
+    audio::{ PlaybackSettings, SpatialListener, Volume, SpatialScale },
     core::Name,
     ecs::{ entity::Entity, query::Added, system::{ Commands, Query, Res } },
     log::info,
 };
 use bevy_ecs_ldtk::{ prelude::LdtkFields, EntityInstance };
+use rand::{ Rng, thread_rng };
 
+use super::delayed_audio_source::DelayedPlaybackSettings;
 use crate::components::player::Player;
 
 /// Insert SpatialListener into the Player entity
@@ -22,25 +26,35 @@ pub fn insert_spatial_listener(
 }
 
 /// Insert SFX AudioBundle into the LDtk EntityInstance with a `sfx` field
-/// FIXME ⚠️ remove debug code and Name query
 pub fn insert_audio_sources(
     mut commands: Commands,
-    added_player: Query<(Entity, &EntityInstance, &Name), Added<bevy_ecs_ldtk::EntityInstance>>,
+    added_player: Query<(Entity, &EntityInstance), Added<bevy_ecs_ldtk::EntityInstance>>,
     asset_server: Res<AssetServer>
 ) {
-    for (entity, ldtk_instance, name) in &added_player {
+    if added_player.is_empty() {
+        return;
+    }
+    let mut rng = thread_rng();
+    for (entity, ldtk_instance) in &added_player {
         if let Ok(sfx) = ldtk_instance.get_string_field("sfx") {
             let sfx_path = format!("sfx/{}", sfx);
+            let volume = match ldtk_instance.get_float_field("volume") {
+                Ok(&v) => Volume::new(v),
+                Err(_) => Volume::default(),
+            };
             let spatial_scale = ldtk_instance
                 .get_float_field("spatial_scale")
                 .ok()
                 .map(|s| SpatialScale::new(super::AUDIO_SCALE * s));
-            info!("Inserted SFX {} for {}", sfx, name);
+            let source = asset_server.load(sfx_path);
+            let delay: Duration = Duration::from_secs_f32(rng.gen_range(0.0..3.0));
 
-            commands.entity(entity).insert(AudioBundle {
-                source: asset_server.load(sfx_path),
+            commands.entity(entity).insert(DelayedPlaybackSettings {
+                source,
+                delay,
                 settings: PlaybackSettings {
                     spatial: true,
+                    volume,
                     spatial_scale,
                     ..PlaybackSettings::LOOP
                 },
