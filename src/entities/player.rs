f5b15f1decb0a3988abccfc55ace9795b86a3c01
Gaeros <163143799+elias-gaeros@users.noreply.github.com>
2024-06-18 16:44:26 +0200

    input manager

--- src/entities/player.rs
+++ src/entities/player.rs
@@ -1,14 +1,17 @@
 use bevy::{ ecs::{ bundle::Bundle, component::Component }, sprite::SpriteBundle };
 use bevy_ecs_ldtk::{ EntityInstance, LdtkEntity, Worldly };
 
-use crate::components::{
-    armor::Armor,
-    health::Health,
-    collision::ColliderBundle,
-    ground::GroundDetection,
-    items::Items,
-    climbing::Climber,
-    swimming::Swimmer,
+use crate::{
+    components::{
+        armor::Armor,
+        health::Health,
+        collision::ColliderBundle,
+        ground::GroundDetection,
+        items::Items,
+        climbing::Climber,
+        swimming::Swimmer,
+    },
+    plugins::input,
 };
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Component)]
@@ -36,4 +39,14 @@ pub struct PlayerBundle {
     // The whole EntityInstance can be stored directly as an EntityInstance component
     #[from_entity_instance]
     entity_instance: EntityInstance,
+
+    // Input manager components
+    #[with(make_action_map)]
+    input_map: input::InputMap,
+    action_state: input::ActionState,
+    action_timers: input::ActionTimers,
+}
+
+fn make_action_map(_: &EntityInstance) -> input::InputMap {
+    input::make_action_map()
 }
